d077e51ac43fb0115a49855e35c6ccaf30a20717
AnasMoShams <anas@Anas.>
2025-07-11 22:15:32 +0300

    New updata, improve display for DataFrame and min, max

--- pandas.py
+++ pandas.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import numpy as np 
 class Series:
     def __init__(self, data, _index=None):
         self.data = data
@@ -86,16 +87,15 @@ print("Values in series is ",series.values())
 
 print("#" * 20)
 
-
 class Dataframe:
+    __supported_type=np.ndarray|Series|list
     def __init__(self, columns, data):
-        if not (type(data) == list and all(type(element) == list for element in data)):
-            raise TabError("only listes")
-        if not (type(columns) in [list, str] and all(type(element) in [list, str] for element in columns)):
-            raise TabError("only list or string")
+        # very slow check 
+        if  not (isinstance(data,self.__supported_type) and isinstance(columns,self.__supported_type)) :
+            raise TabError("only thoese types are supported",self.__supported_type)
         else:
             self.data = data
-            self.columns = columns
+            self.columns = data[0] if columns == None else columns 
 
     def head(self, h=5):
         return (f"\n{self.columns[:h]} \n{self.data[:h]}")
@@ -106,28 +106,26 @@ class Dataframe:
     def mean(self):
         rsl = []
         for element in self.data:
-            # print("main loop ", element)
             for _ in element:
-                #  print("inner loop ", _)
                  rsl.append(_)
-        # print("list of resault ", rsl)
         return sum(rsl) / len(rsl)
     
     def max(self):
-        rsl = []
-        for _ in self.data:
-            rsl.append(max(_))
-        return max(rsl)
+        # rsl = []
+        # for _ in self.data:
+        #     rsl.append(max(_))
+        # return max(rsl)
+        return max(sum(self.data, []))
     
     def min(self):
-        counter = 0
-        rsl = []
-        while counter < len(self.data):
-            rsl.append(min(self.data[counter]))
-            # print("rsl", rsl)
-            counter += 1   
-        return min(rsl)   
-      
+        # counter = 0
+        # rsl = []
+        # while counter < len(self.data):
+        #     rsl.append(min(self.data[counter]))
+        #     counter += 1   
+        # return min(rsl)  
+        return min(sum(self.data, []))
+    
     def median(self):
         rsl = []
         mid = []
@@ -135,7 +133,6 @@ class Dataframe:
             for _ in elemnt:
                 rsl.append(_)
         rsl.sort()
-        # print(rsl)
         if len(rsl) % 2 ==0:
             mid.append(rsl[len(rsl)// 2 - 1])
             mid.append(rsl[len(rsl)//2])
@@ -161,7 +158,34 @@ class Dataframe:
     def index(self):
         return list(range(len(self.data)))
     
-df = Dataframe(["a", "B", "c"],[[1, 2, 3], [4, 5, 6], [7, 8, 9, 3]])
+    def __length(self):
+        for i in range(len(self.data)):
+            if len(self.columns) >= len(self.data[i]):
+                while len(self.data[i]) < len(self.columns):
+                    self.data[i].append(None)
+            else:
+                raise TypeError("the length of data is bigger than length of columns")
+        return self.columns, self.data
+    
+    def display(self):
+        self.columns, self.data = self.__length()
+        rsl = {}
+        for i, col in enumerate(self.columns):
+            values = []
+            for element in self.data:
+                values.append(element[i])
+            rsl[col] = values
+            
+        print("     ".join(rsl.keys()))
+
+        for i in range(len(next(iter(rsl.values())))):
+            rows = []
+            for _ in rsl:
+                rows.append(str(rsl[_][i]))
+            print("     ".join(rows))
+
+    
+df = Dataframe(["a", "B", "c"],[[1, 2, 10], [4, 5, 6], [7, 8, 120]])
 print("The Head for data with name of columns",df.head(2))
 print("The Tail for data with name of columns",df.tail(2))
 print("The mean of data",df.mean())
@@ -170,9 +194,42 @@ print("The minmum number of data",df.min())
 print("The median number of data",df.median())
 print("The length of data", df.count())
 print("The Shape of Data Frame", df.shape()) 
-print("index", df.index())   
-# c = Counter([1, 2, 8, 9, 18, 2, 8, 3, 9, 10, 18, 15, 1, 1, 0, 0, 0])
-# print(c.most_common(1))
-# print(c.most_common(1)[0])
-# print(c.most_common(1)[0][0])
-# print(c.most_common)
+print("index", df.index()) 
+print("Your DataFram ")
+df.display() 
+
+
+# def length(columns, data):
+#     for i in range(len(data)):
+#         # print("element in data",data[i])
+#         if len(columns) >= len(data[i]):
+#             while len(data[i]) < len(columns):
+#                 # print("data",data)
+#                 data[i].append("Nan")
+#         else:
+#             raise TypeError("the length of data is biger than length of columns")
+#     return(columns, data)
+    
+
+# def display(columns, data):
+#     columns, data = length(columns, data)
+#     rsl = {}
+#     for i,col in enumerate(columns):
+#         values = []
+#         for elemnet in data:
+#             values.append(elemnet[i])
+#         rsl[col] = values
+    
+#     print("     ".join(rsl.keys()))
+
+#     for i in range(len(next(iter(rsl.values())))):
+#         rows = []
+#         for col in rsl:
+#             # print("col", col)
+#             rows.append(str(rsl[col][i]))
+#             # print("form in",str(rsl[col][i]))
+#         print("     ".join(rows))
+
+
+# display(["a", "b", "c"], [[1, 2, 3], [4, 5, 6]])
+
